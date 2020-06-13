22f955edbab0476a3f8fcfc485aea815e16a3d27
Kumar Ramanathan <kumar_ramanathan@intuit.com>
2020-06-13 14:57:54 -0700

    Simplified fura unzip and added brew tab for fonts

--- fonts/install.sh
+++ fonts/install.sh
@@ -3,12 +3,12 @@ URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/FiraCode.z
 
 install() {
 	curl -L -s -o /tmp/fura.zip "$URL"
-	unzip /tmp/fura.zip -d /tmp
-	cp /tmp/FiraCode/*.ttf "$2"
+	unzip -j /tmp/fura.zip '*/ttf/*' -d "$1"
 }
 
 if [ "$(uname -s)" = "Darwin" ]; then
-	if which brew >/dev/null 2>&1; then
+	if command -v brew >/dev/null 2>&1; then
+		brew tap homebrew/cask-fonts
 		brew cask list font-firacode-nerd-font >/dev/null 2>&1 || brew cask install font-firacode-nerd-font
 		brew cask list font-firacode-nerd-font-mono >/dev/null 2>&1 || brew cask install font-firacode-nerd-font-mono
 	else
